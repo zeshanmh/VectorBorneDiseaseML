bcfaf5f14bae155b565b0a40bfa2ab89ec9392fb
Zeshan Hussain <zeshanmh@stanford.edu>
2015-12-21 15:11:24 +0530

    commented preprocessing

--- src/util/preprocessing.py
+++ src/util/preprocessing.py
@@ -3,10 +3,31 @@ import pandas as pd
 import math 
 import os
 
-
+NUM_REGIONS = 35
 NUM_YEARS = 20
 
 def create_data_matrices(basepath='../data'): 
+	"""
+	Loads the data into a dataframe and parses it into an input 
+	and output matrix.
+
+	Args:
+		basepath: base path of data; default is '../data'
+
+	Returns:
+		X_full: input matrix (unflattened); dimension of input 
+		matrix is (NUM_REGIONS, NUM_FEATURES, NUM_YEARS), where 
+		NUM_REGIONS = 35, NUM_FEATURES = ?, NUM_YEARS = 20
+
+		y_full: input label matrix (unflattened); dimension of
+		input label matrix is (NUM_REGIONS, NUM_YEARS). Consider
+		only last column of matrix
+
+		region_dictionary: dictionary where key is index and value is
+		name of state in India corresponding to the index
+
+	"""
+
 	#load data as dataframe 
 	df = pd.read_csv(basepath + '/malaria_endemicity_data.csv')
 	
@@ -23,10 +44,36 @@ def create_data_matrices(basepath='../data'):
 
 
 def flatten_matrices(y_full): #need to add X
+	"""
+	Flattens y_full and X_full. We essentially treat the API at each 
+	year for each region as an independent example.
+
+	Args:
+		X_full: input matrix (unflattened); dimension of input 
+		matrix is (NUM_REGIONS, NUM_FEATURES, NUM_YEARS), where 
+		NUM_REGIONS = 35, NUM_FEATURES = ?, NUM_YEARS = 20
+		
+		y_full: input label matrix (unflattened); dimension of
+		input label matrix is (NUM_REGIONS, NUM_YEARS).
+
+	Returns:
+		X_flatten: flattened input matrix whose dimensions are 
+		(NUM_REGIONS*NUM_YEARS, NUM_FEATURES)
+
+		y_flatten: flattened input labels whose dimensions are 
+		(NUM_REGIONS*NUM_YEARS,)
+
+	"""
+
 	return y_full.flatten()	
 
 
 def get_region_value(idx):
+	"""
+	Computes the corresponding index value of the region 
+	in the dictionary
+
+	"""
 	return idx / NUM_YEARS + 1
 
 
@@ -36,11 +83,11 @@ def test():
 	#flatten
 	original = y_full[0,:]
 	y_full = flatten_matrices(y_full)
-	new_column = y_full[0:20,]
+	new_column = y_full[0:NUM_YEARS,]
 
 	assert original.any() == new_column.any()
-	assert y_full.shape == (700,)
+	assert y_full.shape == (NUM_REGIONS*NUM_YEARS,)
 
 
 if __name__ == '__main__':
-    test()
+	test()
